1daf580fd860f33eeaae6b4bb8a60c0acc22804b
C1024622 <j.t.gollnick2@newcastle.ac.uk>
2022-10-13 13:26:11 +0100

    Added Data Req. validators.

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Length
 
 
 class RegisterForm(FlaskForm):
-    username = EmailField(validators=[DataRequired])
-    password = PasswordField(validators=[DataRequired])
+    username = EmailField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired(), Length(min=8, max=15)])
     confirm = PasswordField()
     submit = SubmitField()
