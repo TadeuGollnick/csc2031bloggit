71c1ba051feee5ba613cf7ec58ba74a9d72bc8c1
C1024622 <j.t.gollnick2@newcastle.ac.uk>
2022-11-17 14:09:42 +0000

    Custom character check made for password and username

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
+
+
+def character_check(form, field):
+    excluded_chars = "*<", "*%", "*&"
+
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
-    username = EmailField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired(), Length(min=8, max=15)])
+    username = EmailField(validators=[DataRequired(), character_check])
+    password = PasswordField(validators=[DataRequired(), Length(min=8, max=15), character_check])
     confirm = PasswordField()
     submit = SubmitField()
